bb6e88ec4d4bd99a3b89617d3bb6d4066afffccd
YoannAsl <47599801+YoannAsl@users.noreply.github.com>
2021-05-16 13:00:40 +0200

    Added POST test

--- src/__tests__/NewBill.js
+++ src/__tests__/NewBill.js
@@ -43,72 +43,31 @@ describe('Given I am connected as an employee', () => {
 			expect(fileInput.files[0].name).toBe('test.png');
 		});
 	});
+
+	// Test d'integration POST
 	describe('When I am on NewBill Page and submit the form', () => {
-		test('Then it should post the new bill', async () => {
-			// Test d'integration POST
-			const bills = await firebase.get();
-			const newBill = bills.data[0];
-		});
-		test('Then it should create a new bill and render Bills page', () => {
-			Object.defineProperty(window, 'localStorage', {
-				value: localStorageMock,
-			});
-			window.localStorage.setItem(
-				'user',
-				JSON.stringify({ type: 'Employee' })
-			);
-			window.localStorage.setItem(
-				'email',
-				JSON.stringify({ type: 'johndoe@gmail.com' })
-			);
-
-			document.body.innerHTML = NewBillUI();
-
-			const dateInput = screen.getByTestId('datepicker');
-			fireEvent.change(dateInput, { target: { value: '2021-03-08' } });
-			expect(dateInput.value).toBe('2021-03-08');
-
-			const amountInput = screen.getByTestId('amount');
-			fireEvent.change(amountInput, { target: { value: '1' } });
-			expect(amountInput.value).toBe('1');
-
-			const tvaPctInput = screen.getByTestId('pct');
-			fireEvent.change(tvaPctInput, { target: { value: '20' } });
-			expect(tvaPctInput.value).toBe('20');
-
-			const onNavigate = (pathname) => {
-				document.body.innerHTML = ROUTES({ pathname });
+		test('Then it should create a new bill and render Bills page', async () => {
+			const postSpy = jest.spyOn(firebase, 'post');
+			const newBill = {
+				id: '47qAXb6fIm2zOKkLzMro',
+				vat: '80',
+				fileUrl:
+					'https://firebasestorage.googleapis.com/v0/b/billable-677b6.a…f-1.jpg?alt=media&token=c1640e12-a24b-4b11-ae52-529112e9602a',
+				status: 'pending',
+				type: 'Hôtel et logement',
+				commentary: 'séminaire billed',
+				name: 'encore',
+				fileName: 'preview-facture-free-201801-pdf-1.jpg',
+				date: '2004-04-04',
+				amount: 400,
+				commentAdmin: 'ok',
+				email: 'a@a',
+				pct: 20,
 			};
 
-			const newBill = new NewBill({
-				document,
-				onNavigate,
-				firestore: null,
-				localStorage: window.localStorage,
-			});
-
-			const handleChangeFile = jest.fn((e) => {
-				newBill.handleChangeFile(e);
-			});
-
-			const fileInput = screen.getByTestId('file');
-			fileInput.addEventListener('change', handleChangeFile);
-			fireEvent.change(fileInput, {
-				target: {
-					files: [
-						new File(['test.png'], 'test.png', {
-							type: 'image/png',
-						}),
-					],
-				},
-			});
-
-			const form = screen.getByTestId('form-new-bill');
-			const handleSubmit = jest.fn(newBill.handleSubmit);
-			form.addEventListener('submit', handleSubmit);
-			fireEvent.submit(form);
-			expect(handleSubmit).toHaveBeenCalled();
-			expect(screen.getAllByText('Mes notes de frais')).toBeTruthy();
+			const bills = await firebase.post(newBill);
+			expect(postSpy).toHaveBeenCalledTimes(1);
+			expect(bills.data.length).toBe(5);
 		});
 	});
 });
